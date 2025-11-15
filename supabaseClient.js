0257a8d65dcba2c8e86596a1b32a696094c1f3cb
Preeti Kotabagi <01fe23bcs170@kletech.ac.in>
2025-11-15 11:25:38 +0530

    Add files via upload

---
+++ supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+export const supabase = createClient(
+  "https://bgmamzangkewzxlwtotk.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJnbWFtemFuZ2tld3p4bHd0b3RrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMxMzI2NDQsImV4cCI6MjA3ODcwODY0NH0.7hKnBx5SswBQIEsEe5XUTUp2K3286BoBsBZnjKTnSQk"
+);
